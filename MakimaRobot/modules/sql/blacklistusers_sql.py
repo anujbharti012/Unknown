2e645ce0a2e9d86c9089c9693cd0de59dd7afb44
Choco <143774097+anujbharti012@users.noreply.github.com>
2024-12-16 19:03:43 +0530

    Update blacklistusers_sql.py

--- MakimaRobot/modules/sql/blacklistusers_sql.py
+++ MakimaRobot/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from MakimaRobot.modules.sql import BASE, SESSION
+from DazaiRobot.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
