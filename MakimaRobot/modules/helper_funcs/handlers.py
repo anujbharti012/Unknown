ec5708ef61386bacc20263cff158ce59e445e8a1
Choco <143774097+anujbharti012@users.noreply.github.com>
2024-12-16 18:03:17 +0530

    Update handlers.py

--- MakimaRobot/modules/helper_funcs/handlers.py
+++ MakimaRobot/modules/helper_funcs/handlers.py
@@ -8,8 +8,8 @@ from pyrate_limiter import (
 from telegram import Update
 from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler
 
-import DazaiRobot.modules.sql.blacklistusers_sql as sql
-from DazaiRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
+import MakimaRobot.modules.sql.blacklistusers_sql as sql
+from MakimaRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
 if ALLOW_EXCL:
     CMD_STARTERS = ("/", "!")
